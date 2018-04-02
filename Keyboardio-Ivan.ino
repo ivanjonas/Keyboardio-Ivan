63e1d3de4f42e9f9c580fe8fd65c52481eb77b41
ivanjonas <ivanjonasgomes@gmail.com>
2018-04-02 19:30:04 -0400

    refactor langpack to reflect normal plugin organization

--- Keyboardio-Ivan.ino
+++ Keyboardio-Ivan.ino
@@ -19,7 +19,7 @@
 #include "Kaleidoscope-LEDEffect-Rainbow.h"
 #include "Kaleidoscope-LED-Stalker.h"
 #include "Kaleidoscope-DualUse.h"
-#include "src/LangPack-German/LangPack-German.h"
+#include "src/Kaleidoscope-LangPack-German.h"
 
 
 /** This 'enum' is a list of all the macros used by the Model 01's firmware
