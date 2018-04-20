df5c485e66cf6065f64329583aefe5a685b271dc
Ivan Jonas <ivanjonasgomes@gmail.com>
2018-04-20 12:06:40 -0400

    clean comment clutter

--- Keyboardio-Ivan.ino
+++ Keyboardio-Ivan.ino
@@ -22,66 +22,30 @@
 #include "src/Kaleidoscope-LangPack-German.h"
 
 
-/** This 'enum' is a list of all the macros used by the Model 01's firmware
-  * The names aren't particularly important. What is important is that each
-  * is unique.
-  *
-  * These are the names of your macros. They'll be used in two places.
-  * The first is in your keymap definitions. There, you'll use the syntax
-  * `M(MACRO_NAME)` to mark a specific keymap position as triggering `MACRO_NAME`
-  *
-  * The second usage is in the 'switch' statement in the `macroAction` function.
-  * That switch statement actually runs the code associated with a macro when
-  * a macro key is pressed.
+/** Call with `M(MACRO_NAME)` in keymap.
+  * Add to 'switch' statement in the `macroAction` function.
   */
 enum { MACRO_VERSION_INFO
 //   , MACRO_NAME
      };
 
 
-/** The Model 01's key layouts are defined as 'keymaps'. By default, there are two
-  * keymaps: The standard COLEMAK keymap and the "Function layer" keymap.
-  *
-  * Each keymap is defined as a list using the 'KEYMAP_STACKED' macro, built
-  * of first the left hand's layout, followed by the right hand's layout.
-  *
-  * Keymaps typically consist mostly of `Key_` definitions. There are many, many keys
-  * defined as part of the USB HID Keyboard specification. You can find the names
-  * (if not yet the explanations) for all the standard `Key_` defintions offered by
-  * Kaleidoscope in these files:
+/** The Model 01's key layouts are defined as 'keymaps'. `Key_` definition references:
   *    https://github.com/keyboardio/Kaleidoscope/blob/master/src/key_defs_keyboard.h
   *    https://github.com/keyboardio/Kaleidoscope/blob/master/src/key_defs_consumerctl.h
   *    https://github.com/keyboardio/Kaleidoscope/blob/master/src/key_defs_sysctl.h
   *    https://github.com/keyboardio/Kaleidoscope/blob/master/src/key_defs_keymaps.h
   *
-  * Additional things that should be documented here include
-  *   using ___ to let keypresses fall through to the previously active layer
-  *   using XXX to mark a keyswitch as 'blocked' on this layer
-  *   using ShiftToLayer() and LockLayer() keys to change the active keymap.
-  *   the special nature of the PROG key
-  *   keeping NUM and FN consistent and accessible on all layers
+  * Special symbols:
+  *   `___` to let a keypress fall through to the previously active layer
+  *   `XXX` to disable a keyswitch on this layer
+  *   `ShiftToLayer()` and `LockLayer()` keys to change the active keymap.
   *
-  * The "keymaps" data structure is a list of the keymaps compiled into the firmware.
-  * The order of keymaps in the list is important, as the ShiftToLayer(#) and LockLayer(#)
-  * macros switch to key layers based on this list.
-  *
-  * A key defined as 'ShiftToLayer(FUNCTION)' will switch to FUNCTION while held.
-  * Similarly, a key defined as 'LockLayer(NUMPAD)' will switch to NUMPAD when tapped.
-  *
-  * Layers are "0-indexed" -- That is the first one is layer 0. The second one is layer 1.
-  * The third one is layer 2.
-  * This 'enum' lets us use names like COLEMAK, FUNCTION, and NUMPAD in place of
-  * the numbers 0, 1 and 2.
+  * Layers are "0-indexed". This 'enum' lets us use names like 
+  * COLEMAK, FUNCTION, and NUMPAD in place of the numbers 0, 1 and 2.
   */
 enum { COLEMAK, LAZYNUMBERS, PROGRAMMING, FUNCTION }; // layers
 
-/** This comment temporarily turns off astyle's indent enforcement
-  * so we can make the keymaps actually resemble the physical key layout better
-  */
-// *INDENT-OFF*
-
-/** Convenience definitions.
- */
 #define LANG(n) (Key){.raw = kaleidoscope::language::n}
 
 #define ANGLE_L LSHIFT(Key_Comma)
@@ -101,7 +65,7 @@ enum { COLEMAK, LAZYNUMBERS, PROGRAMMING, FUNCTION }; // layers
 #define TRACK_NEXT  Consumer_ScanNextTrack
 #define PLAY_PAUSE  Consumer_PlaySlashPause
 
-
+// *INDENT-OFF*
 const Key keymaps[][ROWS][COLS] PROGMEM = {
 
   [COLEMAK] = KEYMAP_STACKED
@@ -163,8 +127,6 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
                        ___, ___, Key_Enter, ___,
                        ___)
 };
-
-/* Re-enable astyle's indent enforcement */
 // *INDENT-ON*
 
 
@@ -180,16 +142,6 @@ static void versionInfoMacro(uint8_t keyState) {
 }
 
 
-/** macroAction dispatches keymap events that are tied to a macro
-  * to that macro. It takes two uint8_t parameters.
-  *
-  * The first is the macro being called (the entry in the 'enum' earlier in this file).
-  * The second is the state of the keyswitch. You can use the keyswitch state to figure out
-  * if the key has just been toggled on, is currently pressed or if it's just been released.
-  *
-  * The 'switch' statement should have a 'case' for each entry of the macro enum.
-  * Each 'case' statement should call out to a function to handle the macro in question.
-  */
 const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
   switch (macroIndex) {
 
@@ -201,69 +153,33 @@ const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
 }
 
 
-/** The 'setup' function is one of the two standard Arduino sketch functions.
-  * It's called when your keyboard first powers up. This is where you set up
-  * Kaleidoscope and any plugins.
-  */
 void setup() {
-  // First, call Kaleidoscope's internal setup function
   Kaleidoscope.setup();
 
-  // Next, tell Kaleidoscope which plugins you want to use.
-  // The order can be important. For example, LED effects are
-  // added in the order they're listed here.
   Kaleidoscope.use(
-    // The boot greeting effect pulses the LED button for 10 seconds after the keyboard is first connected
     &BootGreetingEffect,
-
-    // LEDControl provides support for other LED modes
     &LEDControl,
-
-    // We start with the LED effect that turns off all the LEDs.
     &LEDOff,
-
-    // The stalker effect lights up the keys you've pressed recently
     &StalkerEffect,
-
-    // The rainbow effect changes the color of all of the keyboard's keys at the same time
-    // running through all the colors of the rainbow.
     &LEDRainbowEffect,
-
-    // The rainbow wave effect lights up your keyboard with all the colors of a rainbow
-    // and slowly moves the rainbow across your keyboard
     &LEDRainbowWaveEffect,
 
     &DualUse,
 
     &German,
 
-    // The macros plugin adds support for macros
     &Macros
   );
 
-  // We set the brightness of the rainbow effects to 150 (on a scale of 0-255)
-  // This draws more than 500mA, but looks much nicer than a dimmer effect
-  LEDRainbowEffect.brightness(150);
+  LEDRainbowEffect.brightness(150); // scale of 0-255
   LEDRainbowWaveEffect.brightness(150);
 
-  // The LED Stalker mode has a few effects. The one we like is
-  // called 'BlazingTrail'. For details on other options,
-  // see https://github.com/keyboardio/Kaleidoscope-LED-Stalker
   StalkerEffect.variant = STALKER(BlazingTrail);
 
-  // We want to make sure that the firmware starts with LED effects off
-  // This avoids over-taxing devices that don't have a lot of power to share
-  // with USB devices
-  LEDOff.activate();
+  LEDOff.activate(); // avoids over-taxing underpowered devices
 }
 
 
-/** loop is the second of the standard Arduino sketch functions.
-  * As you might expect, it runs in a loop, never exiting.
-  *
-  * For Kaleidoscope-based keyboard firmware, you usually just want to
-  * call Kaleidoscope.loop(); and not do anything custom here.
-  */
 void loop() {
   Kaleidoscope.loop();
 }
